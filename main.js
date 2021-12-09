915a0ab7420df85a83176f599b7600ff7cd6ba59
krisha232 <78255450+krisha232@users.noreply.github.com>
2021-12-09 16:56:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+img="";
+status1="";
+object=[];
+
+function preload(){
+    img=loadImage('dog_cat.jpg')
+}
+
+function setup(){
+canvas=createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "Status = Detecting Objects";
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status1=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object=results;
+}
+
+function draw(){
+    image(img,0,0,640,420);
+
+    if(status1 !=""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status1").innerHTML="Status : Object Detected ";
+            fill('#FF0000');
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+"" + percent + "%",objects[i].x+15,objecsts[i].y+15);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
